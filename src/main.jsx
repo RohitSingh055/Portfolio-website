1023998fd6f0e1c7554a3cdf092a1a340432abeb
RohitSingh055 <rohitrsn055@gmail.com>
2024-09-13 21:39:03 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Navbar from "./components/Navbar.jsx";
 import HomePage from "./Pages/HomePage.jsx";
 import ProjectPage from "./Pages/ProjectPage.jsx";
 import NotFoundPage from "./Pages/NotFoundPage.jsx";
